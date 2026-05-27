73047a31d12124f48fe25011d733393f2cc49818
Sofia <143623338+Veld0ra@users.noreply.github.com>
2026-05-27 10:37:35 -0300

    Fix indentation and syntax errors in FaceEngine

--- face/engine.py
+++ face/engine.py
@@ -4,13 +4,23 @@ from face.animations import AnimationPlayer
 from face.expressions import get_expression
 
 class FaceEngine:
-  def__init__(self, scren):
-    self.screen = scren
-    self.player = AnimationPlayer()
-    self.expression = "sleep"
-  def update(self, state, dt);
-    self.player.update(state, dt)
-    self.expression = self.player.current_expression
-  def draw(self):
-    expression = get_expression(self.expression)
-    expression(self.screen)
+
+    def __init__(self, screen):
+
+        self.screen = screen
+
+        self.player = AnimationPlayer()
+
+        self.expression = "sleep"
+
+    def update(self, state, dt):
+
+        self.player.update(state, dt)
+
+        self.expression = self.player.current_expression
+
+    def draw(self):
+
+        expression = get_expression(self.expression)
+
+        expression(self.screen)
